85fb4ba1b7a2839df928802b91ec1c6c390d5c1a
yesjjin99 <yesjjin99@naver.com>
2022-07-05 00:09:07 +0900

    Add django cors headers
    설치된 패키지들의 버전 업데이트

--- momu-server/settings/base.py
+++ momu-server/settings/base.py
@@ -30,6 +30,7 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
 
@@ -40,12 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'user',
     'feed',
     'rest_framework',
 ]
 
+# cors headers
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
