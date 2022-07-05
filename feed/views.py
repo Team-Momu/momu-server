3b5e04b0825feccc9436f6feea732dbb955a44b1
doyeoo <ehdusla013@gmail.com>
2022-07-05 23:57:42 +0900

    Feat get place list by keyword

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from rest_framework import views
@@ -5,4 +7,21 @@ from rest_framework.status import *
 from rest_framework.response import Response
 from .models import Post, Place, Comment, Scrap
 from .serializers import PlaceSerializer
+from momu.settings import KAKAO_CONFIG
 
+
+class PlaceView(views.APIView):
+    def get(self, request):
+        if 'keyword' in request.GET:
+            keyword = request.GET.get('keyword')
+            rest_api_key = KAKAO_CONFIG['KAKAO_REST_API_KEY']
+
+            url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
+            params = {'query': keyword, 'page': 1}
+            headers = {'Authorization': 'KakaoAK '+rest_api_key}
+
+            data = requests.get(url, params=params, headers=headers).json()['documents']
+
+            return Response(data, status=HTTP_200_OK)
+
+        return Response(status=HTTP_400_BAD_REQUEST)
