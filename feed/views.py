7f506a4c73d342e3b48a8cbba01fe0e6c68385af
doyeoo <ehdusla013@gmail.com>
2022-07-07 12:28:49 +0900

    Feat handle place duplication

--- feed/views.py
+++ feed/views.py
@@ -14,16 +14,16 @@ class PlaceView(views.APIView):
     serializer_class = PlaceSerializer
 
     def get(self, request):
-        size=15
-        page=1
+        size = 15
+        page = 1
         if 'keyword' in request.GET:
             keyword = request.GET.get('keyword')
             rest_api_key = KAKAO_CONFIG['KAKAO_REST_API_KEY']
-            #,
+
             url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
             rect = '126.86417624432379,37.599026970443035,126.962764139611,37.5318164676656'
             params = {'query': keyword, 'category_group_code': 'FD6', 'rect': rect, 'size': size, 'page': page}
-            headers = {'Authorization': 'KakaoAK '+rest_api_key}
+            headers = {'Authorization': 'KakaoAK ' + rest_api_key}
 
             data = requests.get(url, params=params, headers=headers).json()['documents']
             total = requests.get(url, params=params, headers=headers).json()['meta']['total_count']
@@ -32,7 +32,6 @@ class PlaceView(views.APIView):
 
         return Response(status=HTTP_400_BAD_REQUEST)
 
-
     def post(self, request):
         request.data._mutable = True
 
@@ -40,11 +39,17 @@ class PlaceView(views.APIView):
         data['region'] = data['region'].split()[2]
         data['category_name'] = data['category_name'].split(' > ')[1]
 
-        serializer = self.serializer_class(data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'message': '식당 저장 성공', 'place_id': serializer.data.id}, status=HTTP_201_CREATED)
+        if Place.objects.filter(place_id=data['place_id']).exists():
+            place_object = Place.objects.get(place_id=data['place_id'])
+            place_id = self.serializer_class(place_object).data['id']
+            return Response({'message': '이미 존재하는 식당', 'place_id': place_id}, status=HTTP_200_OK)
 
         else:
-            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+            serializer = self.serializer_class(data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': '식당 저장 성공', 'place_id': serializer.data['id']}, status=HTTP_201_CREATED)
+
+            else:
+                return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
