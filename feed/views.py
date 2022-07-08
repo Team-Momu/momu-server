d57d692ea988678033dff25d469256aa5fa341ed
doyeoo <ehdusla013@gmail.com>
2022-07-08 16:32:42 +0900

    Fix feed views.py

--- feed/views.py
+++ feed/views.py
@@ -52,32 +52,3 @@ class PlaceView(views.APIView):
 
             else:
                 return Response({'message': '잘못된 입력값'}, serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
-class PostListView(views.APIView):
-    serializer_class = PostSerializer
-
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = self.serializer_class(posts, many=True)
-
-        return Response(serializer.data, status=HTTP_200_OK)
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
-class PostDetailView(views.APIView):
-    serializer_class = PostSerializer
-
-    def get_object(self, pk):
-        return get_object_or_404(Post, pk=pk)
-
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data, status=HTTP_200_OK)
