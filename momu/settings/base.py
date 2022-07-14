82d2768678f4ca666281d36089426c2ecef156cb
yesjjin99 <yesjjin99@naver.com>
2022-07-15 00:41:26 +0900

    Fix S3 secure option

--- momu/settings/base.py
+++ momu/settings/base.py
@@ -161,7 +161,7 @@ KAKAO_CONFIG = {
 
 # AWS
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SECURE_URLS = False       # use http instead of https
+AWS_S3_SECURE_URLS = True       # use http instead of https
 AWS_QUERYSTRING_AUTH = False
 
 AWS_S3_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
