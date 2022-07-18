00b1c646a8b6ee7d8aef8e34f4b0e010df65dbfe
yesjjin99 <yesjjin99@naver.com>
2022-07-18 18:18:26 +0900

    Add cookie secure, samesite option

--- user/views.py
+++ user/views.py
@@ -113,8 +113,8 @@ class KakaoView(views.APIView):
             'info': user_info_response,
         }, status=status_code)
 
-        response.set_cookie('access_token', access_token, httponly=True)
-        response.set_cookie('refresh_token', refresh_token, httponly=True)
+        response.set_cookie('access_token', access_token, httponly=True, samesite=None, secure=True)
+        response.set_cookie('refresh_token', refresh_token, httponly=True, samesite=None, secure=True)
 
         return response
 
