8e6107dfc02374fb020184584536835a78dff945
yesjjin99 <yesjjin99@naver.com>
2022-08-04 20:36:25 +0900

    Add PostListView Permission
    큐레이션 목록 조회 시 로그인 여부 상관없이 접근 가능하도록 수정

--- user/permissions.py
+++ user/permissions.py
@@ -26,3 +26,48 @@ class UserPermission(permissions.BasePermission):
             return True
         else:
             raise exceptions.AuthenticationFailed(detail='토큰 재발급이 필요합니다')
+
+
+class PostPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            if request.COOKIES.get('access_token'):
+                access_token = request.COOKIES['access_token']
+                payload = jwt.decode(access_token, env('DJANGO_SECRET_KEY'), algorithms=['HS256'])
+                user = get_object_or_404(User, pk=payload['user_id'])
+                request.user = user
+                return True
+            else:
+                raise exceptions.AuthenticationFailed(detail='토큰 재발급이 필요합니다')
+        else:
+            # 피드 조회
+            if request.COOKIES.get('access_token'):
+                access_token = request.COOKIES['access_token']
+                payload = jwt.decode(access_token, env('DJANGO_SECRET_KEY'), algorithms=['HS256'])
+                user = get_object_or_404(User, pk=payload['user_id'])
+                request.user = user
+                return True
+            else:
+                return True
+
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'POST':
+            if request.COOKIES.get('access_token'):
+                access_token = request.COOKIES['access_token']
+                payload = jwt.decode(access_token, env('DJANGO_SECRET_KEY'), algorithms=['HS256'])
+                user = get_object_or_404(User, pk=payload['user_id'])
+                request.user = user
+                return True
+            else:
+                raise exceptions.AuthenticationFailed(detail='토큰 재발급이 필요합니다')
+        else:
+            # 피드 조회
+            if request.COOKIES.get('access_token'):
+                access_token = request.COOKIES['access_token']
+                payload = jwt.decode(access_token, env('DJANGO_SECRET_KEY'), algorithms=['HS256'])
+                user = get_object_or_404(User, pk=payload['user_id'])
+                request.user = user
+                return True
+            else:
+                return True
